999620afdd5e1fa6c07a5f91021ef2e6ad97ee5c
diahkan <143071764+diahkan@users.noreply.github.com>
2023-10-07 10:18:45 +0700

    Add files via upload

---
+++ services/shopService.js
@@ -0,0 +1,63 @@
+const { prisma } = require('../config/prisma');
+
+//get all products
+async function getProducts() {
+  try {
+    const product = await prisma.product.findMany();
+    return product;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// fungsi mendapat fetured product
+async function getFeaturedProducts() {
+  try {
+    const products = await prisma.product.findMany({
+      where: {
+        isFeatured: true
+      },
+      take: 9,
+    })
+    return product;
+  } catch(error) {
+
+  }
+} 
+
+//create product (non accessable for user || admin only)
+async function createProduct(product) {
+  try {
+    const createdProduct = await prisma.product.create({
+      data: {
+        productname: product.productname,
+        brandname: product.brandname,
+        price: product.price,
+        image: product.image
+      }
+    })
+    return createdProduct;
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+// get a product by ID
+async function getProductById(productId) {
+  try {
+    const product = await prisma.product.findUnique({
+      where: {
+        id: Number(productId)
+      }
+    })
+    return product
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+module.exports = {
+  getProducts,
+  createProduct,
+  getProductById
+};
